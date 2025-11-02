81c8acbac290dc5a76b6680c26b249a12159bd88
Kollyns12 <collinsmoses295@gmail.com>
2025-11-02 03:27:59 +0300

    all my modified and new files

---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,18 @@
+// src/pages/ErrorPage.jsx
+import NavBar from "../components/NavBar";
+
+function ErrorPage() {
+  return (
+    <>
+      <header>
+        <NavBar />
+      </header>
+      <main>
+        <h1>Oops! Looks like something went wrong.</h1>
+      </main>
+    </>
+  );
+}
+
+export default ErrorPage;
+
